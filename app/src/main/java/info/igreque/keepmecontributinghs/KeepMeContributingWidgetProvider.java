75306c12cea7b5f2af3f4d0c981ba68340abba92
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2017-10-27 11:24:59 +0900

    (WIP) Create simplest widget.
    But currently eta clashes with an error:
    
    In order, the following will be built (use -v for more details):
     - eta-first-0.1.0.0 (exe:eta-first) (file app\src\main\hs\Info\Igreque\Android.hs changed)
    Preprocessing executable 'eta-first' for eta-first-0.1.0.0..
    Building executable 'eta-first' for eta-first-0.1.0.0..
    [1 of 1] Compiling Main             ( app\src\main\hs\Info\Igreque\Android.hs, C:\Users\igrep\Dropbox\prg\prj\KeepMeContributingHs\dist-newstyle\build\x86_64-windows\eta-0.0.9.2\eta-first-0.1.0.0\c\eta-first\build\eta-first\eta-first-tmp\Main.jar )
    eta.exe: panic! (the 'impossible' happened)
      (Eta version 0.0.9b2):
            genClassMethodParam: Not a valid argument. JIntArray
    
    Please report this as a Eta bug: http://github.com/typelead/eta/issues
    
    :app:etlasNewBuild FAILED
    
    FAILURE: Build failed with an exception.
    
    * What went wrong:
    Execution failed for task ':app:etlasNewBuild'.
    > Process 'command 'etlas'' finished with non-zero exit value 1
    
    * Try:
    Run with --stacktrace option to get the stack trace. Run with --info or --debug option to get more log output.

---
+++ app/src/main/java/info/igreque/keepmecontributinghs/KeepMeContributingWidgetProvider.java
@@ -0,0 +1,12 @@
+package info.igreque.keepmecontributinghs;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class KeepMeContributingWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    }
+}
--- build-hsjar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -xue
-
-(
-  cd hs
-  etlas new-build
-)
-cp "$(find ./hs -name 'eta-first.jar')" app/libs/
